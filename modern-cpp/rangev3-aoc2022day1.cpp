fb2c7ec61f885998a981d022e617103945a85f9c
Konstantin Läufer <laufer@cs.luc.edu>
2023-11-01 18:51:39 -0500

    made range v3 test self-contained, fixes #1
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- modern-cpp/rangev3-aoc2022day1.cpp
+++ modern-cpp/rangev3-aoc2022day1.cpp
@@ -1,6 +1,9 @@
+#include "rangev3-aoc2022day1.hpp"
+
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include <range/v3/view.hpp>
@@ -11,19 +14,23 @@
 
 #include <gtest/gtest.h>
 
-std::pair<int, int> most_nutritious_inventories(std::string_view const filename) {
-  using namespace ranges;
+// std::pair<int, int> most_nutritious_inventories(std::string_view const filename) {
+//   using namespace ranges;
  
-  auto line_to_int = [](auto const& s) { return s.empty() ? 0 : std::stoi(s); };
+//   auto line_to_int = [](auto const& s) { return s.empty() ? 0 : std::stoi(s); };
   
-  // read lines into vector to support chunk_by, which requires a forward range
-  std::ifstream input{filename.data()};
-  std::vector<int> data;
-  copy(
-    getlines(input) | views::transform(line_to_int),
-    back_inserter(data)
-  );
+//   // read lines into vector to support chunk_by, which requires a forward range
+//   std::ifstream input{filename.data()};
+//   std::vector<int> data;
+//   copy(
+//     getlines(input) | views::transform(line_to_int),
+//     back_inserter(data)
+//   );
+// }
 
+using namespace ranges;
+
+std::pair<int, int> most_nutritious_inventories(std::vector<int> data) {
   auto has_no_zeroes = [](auto const& xs) {
     return none_of(xs, [](auto const x) { return x == 0; });
   };
@@ -41,21 +48,19 @@ std::pair<int, int> most_nutritious_inventories(std::string_view const filename)
   auto constexpr n = 3;
   std::vector<int> top_n;
 
+  // retrieve top n inventory values
   for_each(views::ints(0, n), [&](auto i) {
     top_n.push_back(front(inventories));
     pop_heap(inventories);
     inventories.pop_back();
   });
 
+  // return top and sum of top n inventory values
   return std::make_pair(front(top_n), accumulate(top_n, 0));
 }
 
-TEST(AOC2022Day1, Example) {
-  EXPECT_EQ(most_nutritious_inventories("data/day1example.txt"), std::pair(24000, 45000));
-}
-
-TEST(AOC2022Day1, RealData) {
-  auto const result = most_nutritious_inventories("data/day1input.txt");
-  std::cout << "Day 1 part 1 (max) = " << result.first << "\n";
-  std::cout << "Day 1 part 2 (sum) = " << result.second << "\n";
-}
+// TEST(AOC2022Day1, RealData) {
+//   auto const result = most_nutritious_inventories("data/day1input.txt");
+//   std::cout << "Day 1 part 1 (max) = " << result.first << "\n";
+//   std::cout << "Day 1 part 2 (sum) = " << result.second << "\n";
+// }
---
+++ modern-cpp/rangev3-aoc2022day1.hpp
@@ -0,0 +1,9 @@
+#ifndef RANGEV3_AOC2022DAY1_H
+#define RANGEV3_AOC2022DAY1_H
+
+#include <utility>
+#include <vector>
+
+std::pair<int, int> most_nutritious_inventories(std::vector<int> data);
+
+#endif // RANGEV3_AOC2022DAY1_H
