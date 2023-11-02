a04b983fe09e026792c448283a333f4d777ed234
Konstantin Läufer <laufer@cs.luc.edu>
2023-11-01 19:29:33 -0500

    split range v3 example into main and test
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- modern-cpp/rangev3-aoc2022day1.cpp
+++ modern-cpp/rangev3-aoc2022day1.cpp
@@ -1,7 +1,5 @@
 #include "rangev3-aoc2022day1.hpp"
 
-#include <fstream>
-#include <iostream>
 #include <string>
 #include <utility>
 #include <vector>
@@ -11,22 +9,6 @@
 #include <range/v3/iterator.hpp>
 #include <range/v3/numeric.hpp>
 #include <range/v3/action.hpp>
-
-#include <gtest/gtest.h>
-
-// std::pair<int, int> most_nutritious_inventories(std::string_view const filename) {
-//   using namespace ranges;
- 
-//   auto line_to_int = [](auto const& s) { return s.empty() ? 0 : std::stoi(s); };
-  
-//   // read lines into vector to support chunk_by, which requires a forward range
-//   std::ifstream input{filename.data()};
-//   std::vector<int> data;
-//   copy(
-//     getlines(input) | views::transform(line_to_int),
-//     back_inserter(data)
-//   );
-// }
 
 using namespace ranges;
 
@@ -46,6 +28,8 @@ std::pair<int, int> most_nutritious_inventories(std::vector<int> data) {
   make_heap(inventories);
 
   auto constexpr n = 3;
+  if (inventories.size() < n) return std::make_pair(-1, -1);
+
   std::vector<int> top_n;
 
   // retrieve top n inventory values
@@ -58,9 +42,3 @@ std::pair<int, int> most_nutritious_inventories(std::vector<int> data) {
   // return top and sum of top n inventory values
   return std::make_pair(front(top_n), accumulate(top_n, 0));
 }
-
-// TEST(AOC2022Day1, RealData) {
-//   auto const result = most_nutritious_inventories("data/day1input.txt");
-//   std::cout << "Day 1 part 1 (max) = " << result.first << "\n";
-//   std::cout << "Day 1 part 2 (sum) = " << result.second << "\n";
-// }
---
+++ modern-cpp/rangev3-aoc2022day1_main.cpp
@@ -0,0 +1,34 @@
+#include "rangev3-aoc2022day1.hpp"
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#include <range/v3/view.hpp>
+#include <range/v3/algorithm.hpp>
+#include <range/v3/iterator.hpp>
+#include <range/v3/numeric.hpp>
+#include <range/v3/action.hpp>
+
+using namespace ranges;
+
+// to use, redirect stdin from data/day1input.txt
+int main() {
+  // read lines into vector
+  std::vector<int> data;
+  copy(
+    getlines(std::cin) 
+    | views::transform(
+        [](auto const& s) { return s.empty() ? 0 : std::stoi(s); }
+      ),
+    back_inserter(data)
+  );
+
+  copy(data, std::ostream_iterator<int>(std::cout, " "));
+  std::cout << std::endl;
+
+  auto result = most_nutritious_inventories(data);
+
+  std::cout << "Day 1 part 1 (max) = " << result.first << "\n";
+  std::cout << "Day 1 part 2 (sum) = " << result.second << "\n";
+}
